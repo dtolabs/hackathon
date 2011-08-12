dbd3751b06b03f3205f77b40929dfd1426b2faf5
Greg Schueler <greg.schueler@gmail.com>
2011-08-11 20:24:19 -0700

    Updated rundeck cookbook

--- chef-repo/cookbooks/rundeck/attributes/default.rb
+++ chef-repo/cookbooks/rundeck/attributes/default.rb
@@ -1,2 +1,6 @@
 default[:rundeck][:dir] = "/var/lib/rundeck"
-default[:rundeck][:projdir] = "/var/rundeck/projects"
+default[:rundeck][:projdir] = "/var/lib/rundeck/projects"
+default[:rundeck][:port] = "4440"
+default[:rundeck][:web][:context] = ""
+default[:rundeck][:hostname]=node[:ipaddress]
+default[:rundeck][:nodename]=node[:fqdn]
--- chef-repo/cookbooks/rundeck/recipes/default.rb
+++ chef-repo/cookbooks/rundeck/recipes/default.rb
@@ -8,17 +8,34 @@
 #
 include_recipe "java"
 
+#package "openjdk-6-jdk" do
+#  action :install
+#end
+
+group "rundeck" do
+  gid 2040
+end
+
+user "rundeck" do
+  comment "Rundeck User"
+  uid "2040"
+  gid "rundeck"
+  home "#{node[:rundeck][:dir]}"
+  shell "/bin/bash"
+end
+
+directory "#{node[:rundeck][:dir]}" do
+  owner "rundeck"
+  group "sysadmin"
+  mode "0755"
+  action :create
+end
+
 remote_file "#{node[:rundeck][:dir]}/rundeck-launcher-1.3.0.jar" do
-  source "https://github.com/downloads/dtolabs/rundeck/rundeck-launcher-1.3.0.jar"
+  source "http://build.rundeck.org/job/rundeck-development/lastSuccessfulBuild/artifact/rundeckapp/target/rundeck-launcher-1.3.0.jar"
   owner "rundeck"
   group "sysadmin"
   mode "0644"
-end
-
-execute "install" do
-  cwd "#{node[:rundeck][:dir]}"
-  command "java -jar #{node[:rundeck][:dir]}/rundeck-launcher-1.3.0.jar --install-only -b #{node[:rundeck][:dir]}"
-  creates "#{node[:rundeck][:dir]}/server"
 end
 
 template "/etc/security/limits.conf" do
@@ -28,12 +45,59 @@ template "/etc/security/limits.conf" do
   mode 0755
 end
 
-execute "copy-init" do
-  command "cp #{node[:rundeck][:dir]}/server/sbin/rundeckd /etc/init.d/rundeckd"
-  not_if {File.exists?("/etc/init.d/rundeckd")}
+#execute "sysctl" do
+#  command "sysctl -p"
+#end
+
+execute "install" do
+  user "rundeck"
+  group "sysadmin"
+  cwd "#{node[:rundeck][:dir]}"
+  command "java -jar #{node[:rundeck][:dir]}/rundeck-launcher-1.3.0.jar -d --installonly -b #{node[:rundeck][:dir]}"
+  #creates "#{node[:rundeck][:dir]}/server"
+  #creates "#{node[:rundeck][:dir]}/tools"
+  ##creates "#{node[:rundeck][:dir]}/libext"
+  #creates "#{node[:rundeck][:dir]}/docs"
+end
+
+execute "configure" do
+  user "rundeck"
+  group "sysadmin"
+  cwd "#{node[:rundeck][:dir]}"
+  environment ({ 
+    "RDECK_BASE" => node[:rundeck][:dir], 
+    "RDECK_HOME" => node[:rundeck][:dir], 
+    "JAVA_HOME" => node[:java][:java_home] 
+    })
+  command "#{node[:rundeck][:dir]}/tools/bin/rd-setup -n #{node[:rundeck][:nodename]} -N #{node[:rundeck][:hostname]} -s #{node[:rundeck][:hostname]} --framework.server.port=#{node[:rundeck][:port]}"
+  #creates "#{node[:rundeck][:dir]}/server"
+  #creates "#{node[:rundeck][:dir]}/tools"
+  ##creates "#{node[:rundeck][:dir]}/libext"
+  #creates "#{node[:rundeck][:dir]}/docs"
+end
+
+#execute "copy-init" do
+#  command "cp #{node[:rundeck][:dir]}/server/sbin/rundeckd /etc/init.d/rundeckd"
+#  not_if {File.exists?("/etc/init.d/rundeckd")}
+#end
+
+
+template "#{node[:rundeck][:dir]}/server/config/rundeck-config.properties" do
+  source "rundeck-config.properties.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+template "/etc/init.d/rundeckd" do
+  source "rundeckd.erb"
+  owner "root"
+  group "root"
+  mode 0755
 end
 
 service "rundeckd" do
   supports :status => true, :restart => true, :stop=> true, :start=>true
-  action [:enable,:start]
+#  action [:enable]
+    action [:enable,:start]
 end
