257a1e8e4fb8eb347cfda2a2ec271ff5ba1c54af
Keith Hudgins <greenman@greenman.org>
2011-09-05 20:49:10 -0400

    First big dump of updated cf cookbooks.

--- chef-repo/cookbooks/essentials/recipes/default.rb
+++ chef-repo/cookbooks/essentials/recipes/default.rb
@@ -7,7 +7,7 @@
 #
 %w{apt-utils wget curl libcurl3 bison build-essential zlib1g-dev libssl-dev
    libreadline5-dev libxml2 libxml2-dev libxslt1.1 libxslt1-dev git-core sqlite3 libsqlite3-ruby
-   libsqlite3-dev unzip zip ruby-dev libmysql-ruby libmysqlclient-dev libcurl4-openssl-dev}.each do |p|
+   libsqlite3-dev unzip zip ruby-dev libmysql-ruby libmysqlclient-dev libcurl4-openssl-dev libpq-dev}.each do |p|
   package p do
     action [:install]
   end
@@ -27,14 +27,4 @@ directory File.join(node[:deployment][:config_path], "staging") do
   mode "0755"
   recursive true
   action :create
-end
-
-%w{/var/vcap/db /var/vcap/sys /var/vcap/sys/log /var/vcap/data/cloud_controller/tmp /var/vcap/data/db /var/vcap.local/staging /home/ubuntu/.cloudfoundry/devbox/sys /home/ubuntu/.cloudfoundry/devbox/sys/log/}.each do |d|
-  directory d do
-    owner node[:deployment][:user]
-    group node[:deployment][:group]
-    mode "0755"
-    recursive true
-    action :create
-  end
 end
--- chef-repo/cookbooks/redis/metadata.rb
+++ chef-repo/cookbooks/redis/metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "support@vmware.com"
 license          "Apache 2.0"
 description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.0.1"
+version          "1.0.0"
 
+depends "env"
 depends "runit"
