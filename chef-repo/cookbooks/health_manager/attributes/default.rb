e4b75839d96bab180b26e9c05b0df36b9b85834d
Keith Hudgins <greenman@greenman.org>
2011-09-06 18:12:51 -0400

    testing dependency requirements

--- chef-repo/cookbooks/health_manager/attributes/default.rb
+++ chef-repo/cookbooks/health_manager/attributes/default.rb
@@ -3,3 +3,5 @@ default[:health_manager][:config_file] = "health_manager.yml"
 default[:health_manager][:database][:username] = node[:postgresql][:server_root_user]
 default[:health_manager][:database][:password] = node[:postgresql][:server_root_password]
 default[:health_manager][:local_route] = nil
+
+
