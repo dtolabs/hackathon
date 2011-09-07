ef4900bdc8158b94efed4ea98868fd04df534990
Keith Hudgins <greenman@greenman.org>
2011-09-06 23:58:16 -0400

    try something

--- chef-repo/cookbooks/nats_server/attributes/default.rb
+++ chef-repo/cookbooks/nats_server/attributes/default.rb
@@ -1,3 +1,8 @@
 default[:nats_server][:port] = "4222"
 default[:nats_server][:user] = "nats"
 default[:nats_server][:password] = "nats"
+
+default[:nats][:host] = "localhost"
+default[:nats][:port] = "4222"
+default[:nats][:user] = "nats"
+default[:nats][:password] = "nats"
