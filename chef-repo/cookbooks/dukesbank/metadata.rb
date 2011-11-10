ba9a4396a18769310d10268389743c26f6289bc5
Ubuntu <ubuntu@domU-12-31-39-00-4D-D6.compute-1.internal>
2011-11-10 20:10:40 +0000

    Moved dukesbank cookbook for better namespacing

--- chef-repo/cookbooks/dukesbank/metadata.rb
+++ chef-repo/cookbooks/dukesbank/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Keith Hudgins"
 maintainer_email "keith@dtosolutions.com"
 license          "All rights reserved"
-description      "Installs/Configures dukebank-base"
+description      "Installs/Configures dukebank"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 depends          "java"
